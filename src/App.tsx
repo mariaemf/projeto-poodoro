337d09e333c970e7f30a39a9d486918ddb5e6190
mariaemf <mariaeduardamartinsf@gmail.com>
2022-09-12 21:51:47 -0300

    commit inicial projeto pomodoro

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { Button } from "./assets/Button";
+
+export function App() {
+  return (
+    <>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+    </>
+  );
+}
