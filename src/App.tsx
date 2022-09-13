f2ee6318d896c1d322650286650802ca99bb8a13
mariaemf <mariaeduardamartinsf@gmail.com>
2022-09-13 20:14:02 -0300

    configuracao de temas para a aplicacao com typeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import { ThemeProvider } from "styled-components";
 import { useState } from "react";
 import { Button } from "./assets/Button";
+import { defaultTheme } from "./assets/styles/themes/default";
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant="primary" />
       <Button variant="secondary" />
       <Button variant="success" />
       <Button variant="danger" />
       <Button />
-    </>
+    </ThemeProvider>
   );
 }
