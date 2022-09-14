61f5373a3373e287773514c395291426adddef93
mariaemf <mariaeduardamartinsf@gmail.com>
2022-09-14 20:52:32 -0300

    componentes de rotas criados

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import { ThemeProvider } from "styled-components";
+import { Router } from "./Router";
 import { useState } from "react";
-import { Button } from "./assets/Button";
+import { BrowserRouter } from "react-router-dom";
 import { defaultTheme } from "./assets/styles/themes/default";
 import { GlobalStyle } from "./assets/styles/global";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   );
