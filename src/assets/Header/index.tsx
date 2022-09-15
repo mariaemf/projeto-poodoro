1259c2a7a33699dcc7a4abfd0897333098c1c283
mariaemf <mariaeduardamartinsf@gmail.com>
2022-09-15 19:55:21 -0300

    estilizacao de layout e Header da sessão

--- src/assets/Header/index.tsx
+++ src/assets/Header/index.tsx
@@ -1,12 +1,19 @@
 import { HeaderContainer } from "./styles";
+import { Timer, Scroll } from "phosphor-react";
+import Logo from "../../componentes/Logo.svg";
+import { NavLink } from "react-router-dom";
 
 export function Header() {
   return (
     <HeaderContainer>
-      <span>Logo</span>
+      <img src={Logo} alt="" />
       <nav>
-        <a href="">Timer</a>
-        <a href="">history</a>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
       </nav>
     </HeaderContainer>
   );
--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../../assets/Header";
 import { LayoutContainer } from "./styles";
+import Timer from "../../componentes/Timer.svg";
 
 export function DefaultLayout() {
   return (
