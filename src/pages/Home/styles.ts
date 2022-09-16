0cb1dad5a5df11b32ec13b2783491bd8088ac2ec
mariaemf <mariaeduardamartinsf@gmail.com>
2022-09-15 21:01:26 -0300

    estilizacao do button realizada

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -52,3 +52,26 @@ export const Separator = styled.div`
   display: flex;
   justify-content: center;
 `;
+
+export const StartCountdownButton = styled.button`
+  width: 100%;
+  border: 0;
+  padding: 1rem;
+  border-radius: 8px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  gap: 08rem;
+  font-weight: bold;
+
+  cursor: pointer;
+
+  background: ${(props) => props.theme["green-500"]};
+  color: ${(props) => props.theme["gray-100"]};
+
+  &:hover {
+    background: ${(props) => props.theme["green-700"]};
+  }
+`;
