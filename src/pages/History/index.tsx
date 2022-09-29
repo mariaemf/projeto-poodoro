492cd5a0dbc114d90bd04f35dc2ee4c01b689f44
mariaemf <mariaeduardamartinsf@gmail.com>
2022-09-29 20:53:01 -0300

    formatacao a data de inciio utilizando o método formatDaistanceToNow da biblioteca date-fns.

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,6 @@
 import { useContext } from "react";
+import { formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
 import { CyclesContext } from "../../Context/CyclesContext";
 import { HistoryContainer, Status } from "./styles";
 import { HistoryList } from "./styles";
@@ -25,7 +27,12 @@ export function History() {
                 <tr key={cycle.id}>
                   <td>{cycle.task}</td>
                   <td>{cycle.minutesAmount} minutos</td>
-                  <td>{cycle.startDate.toISOString()}</td>
+                  <td>
+                    {formatDistanceToNow(cycle.startDate, {
+                      addSuffix: true,
+                      locale: ptBR,
+                    })}
+                  </td>
                   <td>
                     {cycle.finishedDate && (
                       <Status statusColor="green">Concluído</Status>
